1a625dca2039d53942e01c4f956521728f0397d9
Robson Lima <rsl.robson@gmail.com>
2022-09-24 18:56:25 -0300

    Implementing MainFragment (View).

--- app/src/main/java/com/robsonlima/simplemvpexample/view/MainFragment.kt
+++ app/src/main/java/com/robsonlima/simplemvpexample/view/MainFragment.kt
@@ -5,15 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.robsonlima.simplemvpexample.R
+import com.robsonlima.simplemvpexample.databinding.FragmentMainBinding
 
-class MainFragment: Fragment() {
+class MainFragment : Fragment() {
+    // View Binding with 'fragment_main.xml' so we get reference to xml layout objects easier.
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        // Inflate the layout for this fragment
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupClickListeners()
+    }
+
+    // Setup the button in our fragment to call getUpdatedText method in viewModel
+    private fun setupClickListeners() {
+        binding.fragmentButton.setOnClickListener {
+
+        }
     }
 }
