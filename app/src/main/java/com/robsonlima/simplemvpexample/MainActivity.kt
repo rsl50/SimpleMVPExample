cc0b11893403b068def1d86786e87a96b8215426
Robson Lima <rsl.robson@gmail.com>
2022-09-24 16:48:41 -0300

    Initial commit for project creation

---
+++ app/src/main/java/com/robsonlima/simplemvpexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.robsonlima.simplemvpexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
